3626088af862bab5930448c6e06cf6762c6f0ac2
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-08-21 23:20:37 +0530

    Added state to store the search term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,35 @@
-import React from 'react';
-
-
+import React from "react";
 
 class SearchBar extends React.Component {
+  // This is a callback function that is called when the user types in the search bar.
+    // Making use of state so this function is no longer needed.
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
 
-    // This is a callback function that is called when the user types in the search bar.
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: "" };
 
     render() {
-        return <div className="ui segment">
-            <form className="ui form">
-                <div className="field">
-                    <label>Search Images</label>
-                    {/* When we pass a method to a prop like this, 
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Search Images</label>
+                        {/* When we pass a method to a prop like this,
                         We do not put a set of parentheses to an event handler like onChange. 
                         If you put it, then the method will be called every time the component is rendered.
                         By leaving of the parentheses, we are passing a reference to this event function.*/}
-                    <input type="text" placeholder="Search..." onChange={this.onInputChange} />
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
                 </div>
             </form>
         </div>
-    };
+        );
+    }
 }
 
 export default SearchBar;
