f55edea58532d7379afe34d865663e6ced806a3c
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-08-31 20:02:28 +0530

    Used map function to display the images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const ImageList = () => {
-    return <div>ImageList</div>;
+const ImageList = (props) => {
+    const images = props.images.map((image) => {
+        return (
+            <div className="col-md-3">
+                <div className="card">
+                    <img src={image.urls.regular} alt={image.alt_description} />
+                </div>
+            </div>
+        )
+    });
+    return <div>{images}</div>;
 };
 
 export default ImageList;
